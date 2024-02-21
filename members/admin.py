e4fc08cdad622b6bdef785d1e3b4afa92661c941
Greg Konush <gkonushev@outlook.com>
2024-02-20 20:36:01 -0800

    Added view layout and one template (#6)

--- members/admin.py
+++ members/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Member, Team
+
+admin.site.register(Member)
+admin.site.register(Team)
--- members/urls.py
+++ members/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("create/", views.create, name="create"),
+    path("<int:member_id>/", views.detail, name="detail"),
 ]
