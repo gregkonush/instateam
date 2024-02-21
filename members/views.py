b3883015e3bbfcb0e08c5244257aa5040f7c2e68
Greg Konush <gkonushev@outlook.com>
2024-02-20 23:18:52 -0800

    Connected form to db save

--- members/views.py
+++ members/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Member
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Member, Team
+from .forms import MemberForm
 
 
 # Create your views here.
@@ -11,6 +12,26 @@ def index(request):
 
 
 def create(request):
+    if request.method == "POST":
+        form = MemberForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            phone_number = form.cleaned_data["phone_number"]
+            role = form.cleaned_data["role"]
+            team = Team.objects.first()
+            member = Member(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                phone_number=phone_number,
+                role=role,
+                team=team,
+            )
+            member.save()
+
+        return HttpResponseRedirect("/members")
     return render(request, "members/create.html")
 
 
