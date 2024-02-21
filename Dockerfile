846307a0fb06890b38a507d51ff2f60f13e87541
Greg Konush <gkonushev@outlook.com>
2024-02-20 18:20:07 -0800

    Add deployment to flyctl

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,25 @@
-FROM python
+ARG PYTHON_VERSION=3.12-slim-bullseye
 
-WORKDIR /app
+FROM python:${PYTHON_VERSION}
 
-# Install pipenv
-RUN pip install pipenv
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
 
-# Copy Pipfile and Pipfile.lock and install dependencies
-COPY Pipfile Pipfile.lock /app/
-RUN pipenv install --deploy --ignore-pipfile
+# install psycopg2 dependencies.
+RUN apt-get update && apt-get install -y \
+  libpq-dev \
+  gcc \
+  && rm -rf /var/lib/apt/lists/*
 
-# Copy the rest of the application code
-COPY . /app/
+RUN mkdir -p /code
+
+WORKDIR /code
+
+RUN pip install pipenv gunicorn
+COPY Pipfile Pipfile.lock /code/
+RUN pipenv install --deploy --system
+COPY . /code
+
+EXPOSE 8000
+
+CMD ["gunicorn", "--bind", ":8000", "--workers", "2", "instateam.wsgi"]
--- instateam/settings.py
+++ instateam/settings.py
@@ -10,17 +10,23 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import environ
+import os
 from pathlib import Path
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
+DEBUG = env("DEBUG")
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(%e!uf6i2-k71m!5(i0nis!bd$gu$=#gi6chk-of4%l_5jdyv&"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,11 +80,8 @@ WSGI_APPLICATION = "instateam.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-    }
+    "default": env.db()
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
